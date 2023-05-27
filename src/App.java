1354fde6922ada9e3651455f05572632bf64a22b
SreypichTO <sreypichdarani2003@gmail.com>
2023-05-27 16:34:55 +0700

    Test

--- src/App.java
+++ src/App.java
@@ -16,6 +16,7 @@ public class App extends Application {
         stage.setTitle("Welcome to Library App");
         stage.setScene(new Scene(root));
         stage.show();
+        stage.show();
         
     }
 }
