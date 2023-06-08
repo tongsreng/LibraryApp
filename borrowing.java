2c04c6b8000af87fdab93b3666e88db9343933dd
Anonymous <126799272+Anonymousdesu@users.noreply.github.com>
2023-06-08 12:30:36 +0700

    Add files via upload

---
+++ borrowing.java
@@ -0,0 +1,39 @@
+package samples.db;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+public class borrowing {
+{
+
+   
+    TextField Book_ID;
+
+   
+    Button Books;
+
+   
+    DatePicker Borrow_Date;
+
+   
+    TextField Borrower_ID;
+
+   
+    DatePicker Due_Date;
+
+    
+    TextField First_name;
+
+    
+    Button Home;
+
+    
+    TextField Last_name;
+
+   
+    Button Submit;
+
+}
+
+}
