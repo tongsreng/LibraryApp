79f56f2be88bd935cc7587f3d948b8ca37bd6de4
Anonymous <126799272+Anonymousdesu@users.noreply.github.com>
2023-06-28 00:59:15 +0700

    Add files via upload

--- src/borrowingController.java
+++ src/borrowingController.java
@@ -1,48 +1,33 @@
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
-// import javafx.fxml.FXML;
-// import javafx.scene.control.Button;
-// import javafx.scene.control.DatePicker;
-// import javafx.scene.control.TextField;
+public class borrowingController {
 
-// public class borrowingController {
+    @FXML
+    private TextField Book_ID1;
 
+    @FXML
+    private DatePicker Borrowdate;
 
-//     @FXML
-//     private TextField Book_ID1;
+    @FXML
+    private DatePicker Due_Date;
 
-//     @FXML
-//     private Button Books;
+    @FXML
+    private TextField First_name;
 
-//     @FXML
-//     private DatePicker Borrow_Date;
+    @FXML
+    private TextField Last_name;
 
-//     @FXML
-//     private TextField Borrower_ID;
+    @FXML
+    private TextField StudentID;
 
-//     @FXML
-//     private DatePicker Due_Date;
+    @FXML
+    private Button Submit;
 
-//     @FXML
-//     private TextField First_name;
+    @FXML
+    private TextField phone;
 
-//     @FXML
-//     private Button Home;
-
-//     @FXML
-//     private TextField Last_name;
-
-//     @FXML
-//     private Button Submit;
-
-//     @FXML
-//     private TextField phone;
-
-//     private Stage stage;
-//     private Scene scene;
-//     private Parent root;
-//     public void switchTo(ActionEvent event) throws IOException{
-// }
-// }
-
-
+}
